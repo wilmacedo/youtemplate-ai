ee9cbf2a57c1a09c0697bc16ae195140da84374e
Wil Macedo <wil.macedo.sa@gmail.com>
2023-09-12 12:58:11 -0300

    feat: :sparkles: add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Github } from "lucide-react";
+
 export default function Home() {
-  return <main>Hello World</main>;
+  return (
+    <main className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">youtemplate.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Develop for 📚 purposes
+          </span>
+
+          <Separator orientation="vertical" className="h-6" />
+
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
 }
